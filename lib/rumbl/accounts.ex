1733a2d999b1ec3c97fbf6161ed461d2ddb0feb0
st1llwater <alokpunjofficial@gmail.com>
2023-02-05 15:51:35 +0530

    feat: auth plug

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -27,6 +27,34 @@ defmodule Rumbl.Accounts do
   end
 
   def create_user(attrs \\ %{}) do
-    %User{} |> User.changeset(attrs) |> Repo.insert()
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def authenticate_by_username_pass(username, pass) do
+    user = get_user_by(username: username)
+
+    cond do
+      user && Pbkdf2.verify_pass(pass, user.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        Pbkdf2.no_user_verify()
+        {:error, :not_found}
+    end
   end
 end
